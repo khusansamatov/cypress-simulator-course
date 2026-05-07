4b7136ec777b885dce02557d77f3a8c6e862dba3
khusan987 <khusan.samatov@muralhealth.com>
2026-05-08 02:03:15 +0500

    Create a test plan

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -3,3 +3,76 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 })
+describe("Cypress Simulator", () => {
+  it("success", () => {
+
+  })
+
+  it("error: invalid command scenario", () => {
+
+  })
+
+  it("warning", ()=> {
+
+  })
+
+  it("error: valid command without parentheses", () => {
+
+  })
+
+  it("help", () => {
+
+  })
+
+  it("maximize/minimize", () => {
+
+  })
+
+  it("logout", () => {
+
+  })
+
+  it("show and hide logout button", () =>{
+
+  })
+
+  it("Running ... state", () => {
+
+  })
+
+  it("Accept cookies", () => {
+
+  })
+
+  it("Decline cookies", () => {
+
+  })
+
+  it("Captcha button states", () => {
+
+  })
+
+  it("Captcha error", () => {
+
+  })
+
+  it("Run button - enabled/disabled states", () => {
+
+  })
+
+  it("Reset textarea on logout and login", () =>{
+
+  })
+
+  it("Disable run button on logout and login", () => {
+
+  })
+
+  it("Reset output on logout and login", () =>{
+
+  })
+
+  it("No cookings banner on the login page", () =>{
+    
+  })
+})
