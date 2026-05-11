46c4fcf842422faac1c035c5226b6300321c6519
khusan987 <khusan.samatov@muralhealth.com>
2026-05-11 21:43:19 +0500

    Implement the declines cookie consent scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -188,4 +188,16 @@ describe.only('Cypress Simulator- Cookies Consent', () =>{
       .its('localStorage.cookieConsent')
       .should('be.equal', 'accepted')
   })
+
+  it('declines on the cookies usage', () =>{
+    cy.get('#cookieConsent')
+      .as('cookieConsentBanner')
+      .find('button:contains("Decline")')
+      .click()
+
+    cy.get('@cookieConsentBanner').should('not.be.visible')
+    cy.window()
+      .its('localStorage.cookieConsent')
+      .should('be.equal', 'declined')
+  })
 })
