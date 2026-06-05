907c1c4e40ee39e211047a0c84147f74654dabe8
khusan987 <khusan.samatov@muralhealth.com>
2026-06-06 02:37:28 +0500

    Update: Avoid failures due to a glitch in the Matrix

--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -20,9 +20,14 @@ describe('Cypress Simulator - A11y Checks', () => {
     //   it("successfully simulates a Cypress command (e.g., cy.log('Yay!'))", () => {
     //     cy.run("cy.log('Yay!')")
   it("successfully simulates a Cypress command (e.g., cy.log('Yay!'))", () => {
-        cy.get('#outputArea', {timeout: 6000})
+    cy.get('textarea[placeholder="Write your Cypress code here..."]')
+      .type("cy.log('Yay!')")
+
+    cy.contains('button', 'Run').click()    
+    
+    cy.get('#outputArea', {timeout: 6000})
           .should('contain', 'Success')
-          .and('contain', 'cy.log("Yay!")')
+          .and('contain', "cy.log('Yay!')")
           .and('be.visible')
         
         cy.checkA11y('.success')
